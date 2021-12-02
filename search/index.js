671365fd40f80a073440ffbc3e24a3af8028609b
Evan Burton <butonevan1@gmaail.com>
2021-12-02 11:04:12 -0700

    querying test account and playlists

--- search/index.js
+++ search/index.js
@@ -7,6 +7,9 @@ google.youtube('v3').playlists.list({
     part: 'snippet',
     channelId: 'UC6kvOk8asYpTnu8H9Wst_Gg'
 }).then((response)=> {
-console.log(response);
+const { data } = response;
+data.items.forEach((item) =>{
+    console.log(`title: ${item.snippet.title}\nDescription:${item.snippet.description}\n`);
+})
 
 }).catch((err)=> console.log(err));
