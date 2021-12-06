c62b8667ff3578bcc5bf474782f4f598e79973ca
Evan Burton <butonevan1@gmaail.com>
2021-12-06 10:38:20 -0700

    added sass

--- search/index.js
+++ search/index.js
@@ -8,8 +8,8 @@ google.youtube('v3').playlists.list({
     channelId: 'UC6kvOk8asYpTnu8H9Wst_Gg'
 }).then((response)=> {
 const { data } = response;
-data.items.forEach((item) =>{
-    console.log(item.snippet.thumbnails.default)
+data.items.map((item) =>{
+    console.log(item.snippet)
    // console.log(`title: ${item.snippet.title}\nDescription:${item.snippet.description}\n`);
 })
 
